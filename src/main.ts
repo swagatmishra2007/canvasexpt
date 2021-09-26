a4e25cabd98bf8c86b8b361be2c262206952fa50
swagat mishra <swagatmishra2007@gmail.com>
2021-09-26 02:01:33 -0700

    unneeded code removed

--- src/main.ts
+++ src/main.ts
@@ -17,17 +17,6 @@ function main() {
   const canvas = <HTMLCanvasElement>document.getElementById('flowers');
   canvas.width = document.body.clientWidth;
   canvas.height = document.body.clientHeight;
-  // const flowers = new InteractiveFlowers(canvas);
-  // const rectangles = new Rectangles(canvas);
-  // configureForDrawCircle(canvas);
-  // drawCircle(canvas, new Point(200, 200), 50);
-  // node = bTree(null, 5, canvas);
-
-  // const btn = document.getElementById('clearBtn');
-  // btn.addEventListener('click', () => {
-  //   //flowers.clearCanvas();
-  //   // rectangles.clearCanvas();
-  // });
   const btn = document.getElementById('add-to-tree');
   btn.addEventListener('click', () => {
     const input = <HTMLInputElement>document.getElementById('tree-input');
@@ -36,7 +25,6 @@ function main() {
     if (value) {
       const context = canvas.getContext('2d');
       context.clearRect(0, 0, canvas.width, canvas.height);
-      // node = bTree(node, value, canvas);
       node = bTree(node, value, canvas);
       displayBTree(node, canvas);
     }
